68ac8a84d0f87b614fd4a9e9f84ebb70ea5f2484
Cynthia Gamarra <cgamarra@fi.uba.ar>
2022-11-26 15:32:14 -0300

    Adding some of our rules

--- engine/base_de_conocimietos_enfermedades.py
+++ engine/base_de_conocimietos_enfermedades.py
@@ -1,161 +1,58 @@
-SINTOMAS = ['fiebre_mayor_37', 'dolor_garganta', 'cansancio', 'anosmia',
-            'ageusia', 'tos_seca', 'cefalea', 'secrecion_nasal',
-            'grupo_riesgo', 'contacto_estrecho', 'cant_contagios_zona']
+from responses import Edad
 
-GRIPE_COMUN = 'Gripe Comun'
-COVID = 'COVID19'
-NEUMONIA = 'Neumonia'
-RESFRIO = 'Resfrio'
-DIAGNOSTICOS = [GRIPE_COMUN,  COVID, NEUMONIA, RESFRIO]
+SINTOMAS = ['edad', 'sexo', 'embarazo_actual', 'embarazo_planificado',
+            'metodo_anticonceptivo', 'enfermedad_patologica', 'controlada',
+            'auscultacion_respiratoria', 'auscultacion_cardiaca',
+            'pulso', 'covid', 'vacunacion', 'enfermedad_grave']
+
+NO_SE_PUEDE_DETERMINAR = 'No se puede determinar la enfermedad con\
+                              los datos de entrada.'
+NO_APTO_MENOR_EDAD = 'NO APTO: No puede realizar prueba si es menor de edad.'
+NO_APTO_MAYOR_EDAD = 'NO APTO: No puede realizar prueba si es menor de edad.'
+NO_APTO_DURANTE_EMBARAZO = 'NO APTO: No puede realizar prueba durante\
+                            el embarazo.'
+NO_APTO_PLANIFICA_EMBARAZO = 'NO APTO: No puede realizar la prueba\
+                              si esta planificando un embarazo.'
+NO_APTO_SIN_METODO_ANTICONCEPTIVO = 'NO APTO: No puede realizar la prueba\
+                                     si no usa métodos anticonceptivos.'
+NO_APTO_CONDICION_FISICA_GRAVE = 'NO APTO: Condición física grave.'
+NO_APTO_CONDICION_RESPIRATORIA_GRAVE = 'NO APTO: Condición respiratoria grave.'
+NO_APTO_CONDICION_CARDIACA_GRAVE = 'NO APTO: Condición cardiaca grave.'
+NO_APTO_PULSO_GRAVE = 'NO APTO: Pulso grave.'
+NO_APTO_PASADO_COVID = 'NO APTO: No se puede realizar el estudio si ya\
+                        tuvo COVID.'
+NO_APTO_VACUNADO_COVID = 'NO APTO: No puede participar del estudio si ya\
+                          tiene una vacuna.'
+NO_APTO_VACUNADO_ANTES = 'NO APTO: Fue vacunade anteriormente'
+NO_APTO_SISTEMA_INMUNOLOGICO = 'NO APTO: Tiene enfermedad grave que\
+                                compromete al sistema inmunológico'
+RECALL_METODO_EFECTIVO = 'RECALL: Si incorpora otro metodo mas efectivo,\
+                          se le llamara a futuro.'
+RECALL_CONDICION_MEJORA = 'RECALL: Si su condición mejora se le volverá\
+                           a llamar.'
+RECALL_NO_CONTROLADA = 'RECALL: No puede realizar la prueba si tiene una\
+                        enfermedad o patología no controlada.'
+APTO_PARTICIPAR = 'APTO: Puede participar en el estudio"'
+
+DIAGNOSTICOS = [NO_SE_PUEDE_DETERMINAR, NO_APTO_MENOR_EDAD, NO_APTO_MAYOR_EDAD,
+                NO_APTO_DURANTE_EMBARAZO, NO_APTO_PLANIFICA_EMBARAZO,
+                NO_APTO_SIN_METODO_ANTICONCEPTIVO,
+                NO_APTO_CONDICION_FISICA_GRAVE,
+                NO_APTO_CONDICION_RESPIRATORIA_GRAVE,
+                NO_APTO_CONDICION_CARDIACA_GRAVE,
+                NO_APTO_PULSO_GRAVE, NO_APTO_PASADO_COVID,
+                NO_APTO_VACUNADO_ANTES, NO_APTO_VACUNADO_COVID,
+                NO_APTO_SISTEMA_INMUNOLOGICO,
+                RECALL_CONDICION_MEJORA, RECALL_NO_CONTROLADA,
+                RECALL_METODO_EFECTIVO, APTO_PARTICIPAR]
 
 
+r1 = {'diagnostico': NO_APTO_MENOR_EDAD,
+      'sintomas': {'edad': Edad.MENOR}}
 
-r1 = {'diagnostico': GRIPE_COMUN,
-      'sintomas': {'fiebre_mayor_37': True,
-                   'dolor_garganta': "NORMAL",
-                   'dif_respirar': "NADA",
-                   'cansancio': "NORMAL",
-                   'anosmia': "NADA",
-                   'ageusia': "NADA",
-                   'tos_seca': "PREEXISTENTE",
-                   'cefalea': True,
-                   'secrecion_nasal': False,
-                   'grupo_riesgo': True,
-                   'contacto_estrecho': "NO",
-                   'cant_contagios_zona': "BAJO"}
-      }
-r2 = {'diagnostico': COVID,
-      'sintomas': {'fiebre_mayor_37': False,
-                   'dolor_garganta': "NADA",
-                   'dif_respirar': "NADA",
-                   'cansancio': "NADA",
-                   'anosmia': "AGUDA",
-                   'ageusia': "AGUDA",
-                   'tos_seca': "NADA",
-                   'cefalea': True,
-                   'secrecion_nasal': False,
-                   'grupo_riesgo': False,
-                   'contacto_estrecho': "CASUAL",
-                   'cant_contagios_zona': "ALTO"}
-      }
-r3 = {'diagnostico': COVID,
-      'sintomas': {'fiebre_mayor_37': True,
-                   'dolor_garganta': "INTENSO",
-                   'dif_respirar': "PREEXISTENTE",
-                   'cansancio': "INTENSO",
-                   'anosmia': "PREEXISTENTE",
-                   'ageusia': "PREEXISTENTE",
-                   'tos_seca': "AGUDA",
-                   'cefalea': False,
-                   'secrecion_nasal': False,
-                   'grupo_riesgo': False,
-                   'contacto_estrecho': "PERMANENTE",
-                   'cant_contagios_zona': "ALTO"}
-      }
-r4 = {'diagnostico': GRIPE_COMUN,
-      'sintomas': {'fiebre_mayor_37': True,
-                   'dolor_garganta': "INTENSO",
-                   'dif_respirar': "NADA",
-                   'cansancio': "INTENSO",
-                   'anosmia': "NADA",
-                   'ageusia': "NADA",
-                   'tos_seca': "AGUDA",
-                   'cefalea': True,
-                   'secrecion_nasal': True,
-                   'grupo_riesgo': True,
-                   'contacto_estrecho': "NO",
-                   'cant_contagios_zona': "SINCASOS"}
-      }
+r2 = {'diagnostico': NO_APTO_MAYOR_EDAD,
+      'sintomas': {'edad': Edad.MAYOR}}
 
-r5 = {'diagnostico': NEUMONIA,
-      'sintomas': {'fiebre_mayor_37': True,
-                   'dolor_garganta': "NADA",
-                   'dif_respirar': "AGUDA",
-                   'cansancio': "NORMAL",
-                   'anosmia': "NADA",
-                   'ageusia': "NADA",
-                   'tos_seca': "AGUDA",
-                   'cefalea': False,
-                   'secrecion_nasal': False,
-                   'grupo_riesgo': False,
-                   'contacto_estrecho': "NO",
-                   'cant_contagios_zona': "ALTO"}
-      }
-
-r6 = {'diagnostico': RESFRIO,
-      'sintomas': {'fiebre_mayor_37': False,
-                   'dolor_garganta': "INTENSO",
-                   'dif_respirar': "AGUDA",
-                   'cansancio': "NADA",
-                   'anosmia': "NADA",
-                   'ageusia': "NADA",
-                   'tos_seca': "NADA",
-                   'cefalea': False,
-                   'secrecion_nasal': True,
-                   'grupo_riesgo': True,
-                   'contacto_estrecho': "CASUAL",
-                   'cant_contagios_zona': "BAJO"}
-      }
-
-r7 = {'diagnostico': COVID,
-      'sintomas': {'fiebre_mayor_37': True,
-                   'dolor_garganta': "NORMAL",
-                   'dif_respirar': "NADA",
-                   'cansancio': "NORMAL",
-                   'anosmia': "AGUDA",
-                   'ageusia': "NADA",
-                   'tos_seca': "PREEXISTENTE",
-                   'cefalea': True,
-                   'secrecion_nasal': True,
-                   'grupo_riesgo': False,
-                   'contacto_estrecho': "CASUAL",
-                   'cant_contagios_zona': "ALTO"}
-      }
-
-r8 = {'diagnostico': COVID,
-      'sintomas': {'fiebre_mayor_37': True,
-                   'dolor_garganta': "NADA",
-                   'dif_respirar': "AGUDA",
-                   'cansancio': "INTENSO",
-                   'anosmia': "NADA",
-                   'ageusia': "NADA",
-                   'tos_seca': "AGUDA",
-                   'cefalea': False,
-                   'secrecion_nasal': True,
-                   'grupo_riesgo': True,
-                   'contacto_estrecho': "PERMANENTE",
-                   'cant_contagios_zona': "ALTO"}
-      }
-
-r9 = {'diagnostico': COVID,
-      'sintomas': {'fiebre_mayor_37': True,
-                   'dolor_garganta': "INTENSO",
-                   'dif_respirar': "AGUDA",
-                   'cansancio': "INTENSO",
-                   'anosmia': "PREEXISTENTE",
-                   'ageusia': "AGUDA",
-                   'tos_seca': "AGUDA",
-                   'cefalea': True,
-                   'secrecion_nasal': False,
-                   'grupo_riesgo': False,
-                   'contacto_estrecho': "PERMANENTE",
-                   'cant_contagios_zona': "BAJO"}
-      }
-
-r10 = {'diagnostico': COVID,
-       'sintomas': {'fiebre_mayor_37': False,
-                    'dolor_garganta': "INTENSO",
-                    'dif_respirar': "NADA",
-                    'cansancio': "NADA",
-                    'anosmia': "NADA",
-                    'ageusia': "NADA",
-                    'tos_seca': "AGUDA",
-                    'cefalea': True,
-                    'secrecion_nasal': True,
-                    'grupo_riesgo': True,
-                    'contacto_estrecho': "NO",
-                    'cant_contagios_zona': "ALTO"}
-      }
 
 class Paciente():
     def __init__(self):
