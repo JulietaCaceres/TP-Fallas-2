536e6e50726bf84576f28ed68151320bf5ecc217
garoblesmeli <gabriel.robles@mercadolibre.com>
2022-12-14 10:17:06 -0300

    age rules and sex improvements

--- engine/base_de_conocimietos.py
+++ engine/base_de_conocimietos.py
@@ -4,27 +4,30 @@ from engine.responses import Edad, Sexo, MetodoAnticonceptivo,\
 from engine.frames import VolunteerFrame, WomanVolunteerFrame, FrameTree
 from engine.evaluations import *
 
-v1 = WomanVolunteerFrame(embarazo_actual=True, evaluation=NO_APTO_DURANTE_EMBARAZO)
-v2 = WomanVolunteerFrame(embarazo_planificado=True, evaluation=NO_APTO_PLANIFICA_EMBARAZO)
-v3 = WomanVolunteerFrame(metodo_anticonceptivo=MetodoAnticonceptivo.NO_USA, evaluation=NO_APTO_SIN_METODO_ANTICONCEPTIVO)
-v4 = WomanVolunteerFrame(metodo_anticonceptivo=MetodoAnticonceptivo.PRESERVATIVO, evaluation=RECALL_METODO_EFECTIVO)
-v5 = VolunteerFrame(examen_fisico=ExamenFisico.GRAVE, evaluation=NO_APTO_CONDICION_FISICA_GRAVE)
-v6 = VolunteerFrame(auscultacion_respiratoria=AuscultacionRespiratoria.GRAVE, evaluation=NO_APTO_CONDICION_RESPIRATORIA_GRAVE)
-v7 = VolunteerFrame(auscultacion_cardiaca=AuscultacionCardiaca.GRAVE, evaluation=NO_APTO_CONDICION_CARDIACA_GRAVE)
-v8 = VolunteerFrame(pulso=Pulso.GRAVE, evaluation=NO_APTO_PULSO_GRAVE)
-v9 = VolunteerFrame(examen_fisico=ExamenFisico.INTERMEDIO, evaluation=RECALL_CONDICION_MEJORA)
-v10 = VolunteerFrame(auscultacion_respiratoria=AuscultacionRespiratoria.INTERMEDIO, evaluation=RECALL_CONDICION_MEJORA)
-v11 = VolunteerFrame(auscultacion_cardiaca=AuscultacionCardiaca.INTERMEDIO, evaluation=RECALL_CONDICION_MEJORA)
-v12 = VolunteerFrame(pulso=Pulso.INTERMEDIO, evaluation=RECALL_CONDICION_MEJORA)
-v13 = VolunteerFrame(covid=True, evaluation=NO_APTO_PASADO_COVID)
-v14 = VolunteerFrame(vacunacion=True, evaluation=NO_APTO_VACUNADO_COVID)
-v15= VolunteerFrame(enfermedad_grave=True, evaluation=NO_APTO_SISTEMA_INMUNOLOGICO)
-v16 = VolunteerFrame(enfermedad_patologica=True, controlada=False, evaluation=RECALL_NO_CONTROLADA)
+f0 = VolunteerFrame(edad=Edad.MENOR, evaluation=NO_APTO_MENOR_EDAD)
+f1 = VolunteerFrame(edad=Edad.MAYOR, evaluation=NO_APTO_MAYOR_EDAD)
+f2 = WomanVolunteerFrame(embarazo_actual=True, evaluation=NO_APTO_DURANTE_EMBARAZO)
+f3 = WomanVolunteerFrame(embarazo_planificado=True, evaluation=NO_APTO_PLANIFICA_EMBARAZO)
+f4 = WomanVolunteerFrame(metodo_anticonceptivo=MetodoAnticonceptivo.NO_USA, evaluation=NO_APTO_SIN_METODO_ANTICONCEPTIVO)
+f5 = WomanVolunteerFrame(metodo_anticonceptivo=MetodoAnticonceptivo.PRESERVATIVO, evaluation=RECALL_METODO_EFECTIVO)
+f6 = VolunteerFrame(examen_fisico=ExamenFisico.GRAVE, evaluation=NO_APTO_CONDICION_FISICA_GRAVE)
+f7 = VolunteerFrame(auscultacion_respiratoria=AuscultacionRespiratoria.GRAVE, evaluation=NO_APTO_CONDICION_RESPIRATORIA_GRAVE)
+f8 = VolunteerFrame(auscultacion_cardiaca=AuscultacionCardiaca.GRAVE, evaluation=NO_APTO_CONDICION_CARDIACA_GRAVE)
+f9 = VolunteerFrame(pulso=Pulso.GRAVE, evaluation=NO_APTO_PULSO_GRAVE)
+f10 = VolunteerFrame(examen_fisico=ExamenFisico.INTERMEDIO, evaluation=RECALL_CONDICION_MEJORA)
+f11 = VolunteerFrame(auscultacion_respiratoria=AuscultacionRespiratoria.INTERMEDIO, evaluation=RECALL_CONDICION_MEJORA)
+f12 = VolunteerFrame(auscultacion_cardiaca=AuscultacionCardiaca.INTERMEDIO, evaluation=RECALL_CONDICION_MEJORA)
+f13 = VolunteerFrame(pulso=Pulso.INTERMEDIO, evaluation=RECALL_CONDICION_MEJORA)
+f14 = VolunteerFrame(covid=True, evaluation=NO_APTO_PASADO_COVID)
+f15 = VolunteerFrame(vacunacion=True, evaluation=NO_APTO_VACUNADO_COVID)
+f16= VolunteerFrame(enfermedad_grave=True, evaluation=NO_APTO_SISTEMA_INMUNOLOGICO)
+f17 = VolunteerFrame(enfermedad_patologica=True, controlada=False, evaluation=RECALL_NO_CONTROLADA)
 
 class InferenceEngine():
 
     def __init__(self):
-        self.conocimiento = FrameTree(volunteer_instances=[v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16])
+        self.conocimiento = FrameTree(volunteer_instances=[f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16, f17])
+
     def get_evaluation_for_volunteer(self, volunteer_data):
         result = self.conocimiento.eval(volunteer_data)
         print("resultado: ", result)
--- engine/frames.py
+++ engine/frames.py
@@ -1,8 +1,11 @@
 from engine.responses import Sexo
+
+
 class FrameTree:
+
     def __init__(self, volunteer_instances):
         self.volunteer_instances = volunteer_instances
-        print("instances: ", volunteer_instances)
+        # print("instances: ", volunteer_instances)
 
     def eval(self, volunteer_data):
         for volunteer_instance in self.volunteer_instances:
@@ -11,10 +14,13 @@ class FrameTree:
                 return volunteer_instance.evaluation
         return "Apto para realizar la prueba"
 
+
 class VolunteerFrame:
-    def __init__(self, sex=Sexo.MASCULINO, enfermedad_patologica=None, controlada=None, examen_fisico=None, auscultacion_respiratoria=None,
+
+    def __init__(self, sex=Sexo.MASCULINO, edad=None, enfermedad_patologica=None, controlada=None, examen_fisico=None, auscultacion_respiratoria=None,
                  auscultacion_cardiaca=None, pulso=None, covid=None, vacunacion=None, enfermedad_grave=None, evaluation=None):
         self.sex = sex
+        self.edad = edad
         self.enfermedad_patologica = enfermedad_patologica
         self.controlada = controlada
         self.examen_fisico = examen_fisico
@@ -30,6 +36,7 @@ class VolunteerFrame:
     def __repr__(self):
         return f"""
         sex = {self.sex}
+        edad = {self.edad}
         enfermedad_patologica = {self.enfermedad_patologica}
         controlada = {self.controlada}
         examen_fisico = {self.examen_fisico}
@@ -43,36 +50,39 @@ class VolunteerFrame:
         """
 
     def es(self, volunteer_data):
-        return ((not self.enfermedad_grave) or self.enfermedad_grave == volunteer_data["enfermedad_grave"]) and \
-                  ((not self.vacunacion) or self.vacunacion == volunteer_data["vacunacion"]) and \
-                    ((not self.covid) or self.covid == volunteer_data["covid"]) and \
-                        ((not self.pulso) or self.pulso == volunteer_data["pulso"]) and \
-                            ((not self.auscultacion_cardiaca) or self.auscultacion_cardiaca == volunteer_data["auscultacion_cardiaca"]) and \
-                                ((not self.auscultacion_respiratoria) or self.auscultacion_respiratoria == volunteer_data["auscultacion_respiratoria"]) and \
-                                    ((not self.examen_fisico) or self.examen_fisico == volunteer_data["examen_fisico"]) and \
-                                            ((not self.enfermedad_patologica) or (self.enfermedad_patologica == volunteer_data["enfermedad_patologica"] and  self.controlada == volunteer_data["controlada"]))
-
+        return ((not self.edad or self.edad == volunteer_data["edad"]) and \
+                ((not self.enfermedad_grave) or self.enfermedad_grave == volunteer_data["enfermedad_grave"]) and \
+                ((not self.vacunacion) or self.vacunacion == volunteer_data["vacunacion"]) and \
+                ((not self.covid) or self.covid == volunteer_data["covid"]) and \
+                ((not self.pulso) or self.pulso == volunteer_data["pulso"]) and \
+                ((not self.auscultacion_cardiaca) or self.auscultacion_cardiaca == volunteer_data["auscultacion_cardiaca"]) and \
+                ((not self.auscultacion_respiratoria) or self.auscultacion_respiratoria == volunteer_data["auscultacion_respiratoria"]) and \
+                ((not self.examen_fisico) or self.examen_fisico == volunteer_data["examen_fisico"]) and \
+                ((not self.enfermedad_patologica) or (self.enfermedad_patologica == volunteer_data["enfermedad_patologica"] and  self.controlada == volunteer_data["controlada"])))
 
 
 class WomanVolunteerFrame(VolunteerFrame):
-    def __init__(self, sex=Sexo.FEMENINO, enfermedad_patologica=None, controlada=None, examen_fisico=None, auscultacion_respiratoria=None,
+
+    def __init__(self, sex=Sexo.FEMENINO, edad=None, enfermedad_patologica=None, controlada=None, examen_fisico=None, auscultacion_respiratoria=None,
                  auscultacion_cardiaca=None, pulso=None, covid=None, vacunacion=None, enfermedad_grave=None, embarazo_actual=None, embarazo_planificado=None,
                  metodo_anticonceptivo=None, evaluation=None):
 
-        super().__init__(sex, enfermedad_patologica, controlada, examen_fisico, auscultacion_respiratoria,
+        super().__init__(sex, edad, enfermedad_patologica, controlada, examen_fisico, auscultacion_respiratoria,
                          auscultacion_cardiaca, pulso, covid, vacunacion, enfermedad_grave, evaluation)
         self.embarazo_actual = embarazo_actual
         self.embarazo_planificado = embarazo_planificado
         self.metodo_anticonceptivo = metodo_anticonceptivo
 
     def es(self, volunteer_data):
-        return (((not self.embarazo_actual) or (self.embarazo_actual == volunteer_data["embarazo_actual"])) \
-               and ((not self.embarazo_planificado) or (self.embarazo_planificado == volunteer_data["embarazo_planificado"])) \
-               and ((not self.metodo_anticonceptivo) or (self.metodo_anticonceptivo == volunteer_data["metodo_anticonceptivo"]))) \
+        return ((Sexo.FEMENINO == volunteer_data["sexo"])
+                and ((not self.embarazo_actual) or (self.embarazo_actual == volunteer_data["embarazo_actual"])) \
+                and ((not self.embarazo_planificado) or (self.embarazo_planificado == volunteer_data["embarazo_planificado"])) \
+                and ((not self.metodo_anticonceptivo) or (self.metodo_anticonceptivo == volunteer_data["metodo_anticonceptivo"]))) \
 
     def __repr__(self):
         return f"""
         sex = {self.sex}
+        edad = {self.edad}
         enfermedad_patologica = {self.enfermedad_patologica}
         controlada = {self.controlada}
         examen_fisico = {self.examen_fisico}
